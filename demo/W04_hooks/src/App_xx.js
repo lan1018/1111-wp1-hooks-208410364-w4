ee9d1f622c10093904aec94c6b286e2b3b7a533e
frankielan168 <frankielan168@gmail.com>
2022-09-28 19:46:07 +0800

    W04-P1: Hooks Tutorial T11_64

---
+++ demo/W04_hooks/src/App_xx.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import T11_xx from './tutorial/1-useState/T11_64-error-example';
+
+const App_xx = () => {
+  return (
+    <div className='container'>
+      <h2>Tutorial 11 - 123456789</h2>
+      <T11_xx />
+    </div>
+  );
+};
+
+export default App_xx;
