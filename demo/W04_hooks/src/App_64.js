58719f6961149d7815b07e6d25ea3971a09ab1b3
frankielan168 <frankielan168@gmail.com>
2022-10-05 21:45:28 +0800

    W05-P4: T31_64: conditional rendering -- 3 cases

--- demo/W04_hooks/src/App_64.js
+++ demo/W04_hooks/src/App_64.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import T23_64 from './tutorial/2-useEffect/T23_64-useEffect-fetch-data';
+import T31_64 from './tutorial/3-conditional-rendering/T31_64-multiple-returns';
 
 const App_64 = () => {
   return (
     <div className='container'>
-      <h2>Tutorial 23 - 208410364</h2>
-      <T23_64 />
+      <h2>Tutorial 31 - 208410364</h2>
+      <T31_64 />
     </div>
   );
 };
