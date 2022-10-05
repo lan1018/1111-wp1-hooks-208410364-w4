a5b7c7fa81ed2bc6aba72fe7b42fb5bf5e1971e8
frankielan168 <frankielan168@gmail.com>
2022-10-05 19:12:48 +0800

    W05-P1: Hooks Tutorial T14_64

--- demo/W04_hooks/src/App_64.js
+++ demo/W04_hooks/src/App_64.js
@@ -1,40 +1,12 @@
-import { useState } from 'react';
-import Menu_64 from './components/Menu_64';
-import items from './data'
 import React from 'react';
-import './App_64.css';
-import Category_64 from './components/Category_64';
-
-const allCategories = ['all', 'breakfast', 'lunch', 'chakes', '64'];
+import T14_64_UseStateObject from './tutorial/1-useState/T14_64-useState-object';
 
 const App_64 = () => {
-  const [menuItems, setMenuItems] = useState(items);
-  const [categories, setCategories] = useState(allCategories);
-
-  const filterItems = (category) => {
-    if(category === 'all') {
-      setMenuItems(items);
-    } else {
-      const newItems = items.filter( (item) => item.category === category);
-      setMenuItems(newItems);
-    }
-  }
-
-  console.log('menuItems', menuItems);
   return (
-    <section className="menu">
-        {/* title */}
-        <div className="title">
-          <h2>our menu 208410364</h2>
-          <div className="underline"></div>
-        </div>
-        {/* filter buttons */}
-        <Category_64 categories={categories} filterItems={filterItems} />
-        {/* menu items */}
-        <Menu_64 items={menuItems} />
-        <script src="app.js"></script>
-      </section>
-       
+    <div className='container'>
+      <h2>Tutorial 14 - 208410380</h2>
+      <T14_64_UseStateObject />
+    </div>
   );
 };
 
